8d0cc688d859223dbfcc6d3721adcb9a203dc6b0
Gustav Sundin <gustav.sundin@wirelesscar.com>
2019-12-03 17:46:26 +0100

    Day 3 - part 1

--- day3/day3.py
+++ day3/day3.py
@@ -4,9 +4,6 @@ grid_size = 10000
 
 
 def manhattan_distance_of_closest_crossing(lines):
-    # grid = [[0 for x in range(grid_size)] for y in range(grid_size)]
-    #for line in lines:
-    #    draw_line(grid, line, 1, 1)
     line1 = draw_line(lines[0])
     line2 = draw_line(lines[1])
 
@@ -19,21 +16,20 @@ def  find_all_crossings(line1, line2):
 
 
 def find_closest_crossing(crossings):
-    min_distance = 1000000
+    distances = []
     for crossing in crossings:
         distance = get_manhattan_distance(crossing)
-        if (distance < min_distance):
-            min_distance = distance
-    return min_distance
+        distances.append(distance)
+        print(crossing, distance)
+    return min(distances)
 
 def get_manhattan_distance(crossing):
-    print(crossing)
-    return crossing[0] + crossing[1] - 2
+    return abs(crossing[0]) + abs(crossing[1])
 
 
 def draw_line(line):
     coordinates = []
-    current_position = (1, 1)
+    current_position = (0, 0)
     for step in line:
         step_coordinates = draw_step(step, current_position[0], current_position[1])
         current_position = step_coordinates[-1]
@@ -48,29 +44,21 @@ def draw_step(step, start_x, start_y):
     coordinates = []
 
     if (direction == "U"):
-        r = range(start_y + 1, start_y + distance)
+        r = range(start_y + 1, start_y + distance + 1)
         for y in r:
             coordinates.append((start_x, y))
-            #grid[start_x][y] += 1
-        #return (start_x, start_y + distance)
     elif (direction == "D"):
-        r = range(start_y - distance, start_y - 1)
+        r = reversed(range(start_y - distance, start_y))
         for y in r:
             coordinates.append((start_x, y))
-            #grid[start_x][y] += 1
-        #return (start_x, start_y - distance)
     elif (direction == "R"):
-        r = range(start_x + 1, start_x + distance)
+        r = range(start_x + 1, start_x + distance + 1)
         for x in r:
             coordinates.append((x, start_y))
-            #grid[x][start_y] += 1
-        #return (start_x + distance, start_y)
     elif (direction == "L"):
-        r = range(start_x - distance, start_x - 1)
+        r = reversed(range(start_x - distance, start_x))
         for x in r:
             coordinates.append((x, start_y))
-            #grid[x][start_y] += 1
-        #return (start_x - distance, start_y)
     else:
         raise RuntimeError('Unknown direction code', direction)
 
