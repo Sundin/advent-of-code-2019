21f6cef39b96eec5b22ffed3ba6713b6832bdb6b
Gustav Sundin <gustav.sundin@wirelesscar.com>
2019-12-06 11:54:25 +0100

    Day 6 - part 1

--- day6/day6.py
+++ day6/day6.py
@@ -1,14 +1,60 @@
 
 def total_number_of_orbits(input):
-    return 1
+    all_planets = {}
+    orbit_pairs = split_into_orbit_pairs(input)
+    for pair in orbit_pairs:
+        planets = split_into_planets(pair)
+        if planets[0] not in all_planets:
+            all_planets[planets[0]] = Planet(planets[0])
+        if planets[1] not in all_planets:
+            all_planets[planets[1]] = Planet(planets[1])
+        all_planets[planets[0]].add_child(all_planets[planets[1]])
+        all_planets[planets[1]].set_parent(all_planets[planets[0]])
+    return count_all_orbits(all_planets)
 
+def count_all_orbits(orbit_map):
+    sum = 0
+    for planet in orbit_map.values():
+        sum += count_orbits(planet)
+    return sum
+        
+def count_orbits(planet):
+    if planet.parent == "":
+        return 0
+    return count_orbits(planet.parent) + 1    
+
+def split_into_planets(orbit_pair):
+    return orbit_pair.split(')')
 
 def split_into_orbit_pairs(input):
     input.replace(" ", "")
     input.replace("\t", "")
     pairs = input.split('\n')
     pairs = list(map(str.strip, pairs))
-    for orbit in pairs:
-        print(".", orbit, ",")
-    print(pairs)
     return pairs
+
+class Planet(object):
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+        self.parent = ""
+    def __repr__(self):
+        return self.name
+    def add_child(self, node):
+        assert isinstance(node, Planet)
+        self.children.append(node)
+    def set_parent(self, node):
+        assert isinstance(node, Planet)
+        self.parent = node
+
+def read_input_file(file_path):
+    with open(file_path) as file:
+        file_contents = file.read()
+        return file_contents
+
+def main():
+    input = read_input_file('day6/input.txt')
+    orbits = total_number_of_orbits(input)
+    print("Answer 1:", orbits)
+
+main()
--- day6/test_day6.py
+++ day6/test_day6.py
@@ -9,9 +9,42 @@ def test_split_into_orbit_pairs():
     ) == ["COM)A", "COM)B"]
 
 
+def test_split_into_planets():
+    assert split_into_planets("A)B") == ["A", "B"]
+
+
 def test_total_number_of_orbits():
     assert total_number_of_orbits("COM)A") == 1
-    # assert total_number_of_orbits(
-    #     """COM)A
-    #     COM)B"""
-    # ) == 2
+    assert total_number_of_orbits(
+        """COM)A
+        COM)B"""
+    ) == 2
+    assert total_number_of_orbits(
+        """COM)A
+        A)B"""
+    ) == 3
+    assert total_number_of_orbits(
+        """A)B
+        COM)A"""
+    ) == 3
+    assert total_number_of_orbits("""COM)B
+    B)C
+    C)D
+    D)E
+    E)F
+    B)G
+    G)H
+    D)I
+    E)J
+    J)K
+    K)L""") == 42
+
+def test_count_orbits():
+    com = Planet("COM")
+    a = Planet("a")
+    b = Planet("b")
+    a.set_parent(com)
+    b.set_parent(a)
+    assert count_orbits(com) == 0
+    assert count_orbits(a) == 1
+    assert count_orbits(b) == 2
