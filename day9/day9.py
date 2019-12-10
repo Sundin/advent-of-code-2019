8639406c2e8f4af89c1a364fdc17d4034bddaacb
Gustav Sundin <sundin666@gmail.com>
2019-12-10 06:49:41 +0100

    segmentation fault

--- day9/day9.py
+++ day9/day9.py
@@ -1,10 +1,16 @@
 from intc_comp import *
+import sys
 
 
 def main():
     program = read_input_file('day9/input.txt')
     output1 = get_intcode_computer_ouput(program, [1])
     print("Answer1:", output1)
+
+    program = read_input_file('day9/input.txt')
+    sys.setrecursionlimit(32000)
+    output2 = get_intcode_computer_ouput(program, [2])
+    print("Answer1:", output2)
 
 
 main()
